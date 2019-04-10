ff62e8dd52843a50ed1514112c6707f3c6e66565
SoulMelody <qq379728921@hotmail.com>
2019-04-10 21:30:05 +0800

    time sync

--- hls_get/remuxer.py
+++ hls_get/remuxer.py
@@ -12,9 +12,11 @@ def remux(in_name, out_name):
         for stream in input_.streams:
             in_to_out[stream] = output.add_stream(template=stream)
 
+        min_dts = min(stream.start_time for stream in in_to_out.values())
+
         for packet in input_.demux(tuple(in_to_out.keys())):
             if packet.dts is not None:
-                packet.dts -= packet.stream.start_time
+                packet.dts -= min_dts
                 packet.stream = in_to_out[packet.stream]
                 output.mux(packet)
 
